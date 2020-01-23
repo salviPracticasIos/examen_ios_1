bec999eb040c62d36437b285c914bcfdc421f8f7
user164187 <user164187@92-63-141-243.rdns.melbourne.co.uk>
2020-01-23 20:17:09 +0000

    first commit

---
+++ Clockwise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clockwise
+//
+//  Created by user164187 on 1/23/20.
+//  Copyright © 2020 Salvador gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
