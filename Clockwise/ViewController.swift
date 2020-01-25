92570aaf58bf5cd3a2ac5989581d4e2281e4d513
user164187 <user164187@92-63-141-243.rdns.melbourne.co.uk>
2020-01-25 15:37:08 +0000

    Ejercicio acacbado

--- Clockwise/ViewController.swift
+++ Clockwise/ViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Declaro 5 variables que son Bool para utilizarlas a modo de interruptor, la primera la utilizo para saber si tengo que cambiar los colores en sentido de las qagujas del relij o en sentido contrario, las otras 4 las utilizo para saber cual de los dos colores que puede tener un boton va a cambiar
     var isClockwise = true
-    var isPressedButton1 = true
-    var isPressedButton2 = true
-    var isPressedButton3 = true
-    var isPressedButton4 = true
+    var isOriginalColor1 = true
+    var isOriginalColor2 = true
+    var isOriginalColor3 = true
+    var isOriginalColor4 = true
     
-    // Declaro las variables de los botones
+    // Uno los botones de la vista con el controlador
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
@@ -26,10 +27,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var infoLabel: UILabel!
     
     // Funcion que cambia la direccion en la que cambian los colores
-    
     @IBAction func changeDirection() {
         
+        // cada vez que pulso el boton de Change direction, cambio el valor de la variable isClockwise
         isClockwise.toggle()
+        
+        // Segun el valor de la variable isClockwise cambio el texto de infoLabel
         if isClockwise {
             infoLabel.text = "Go Clockwise"
         } else {
@@ -38,89 +41,105 @@ class ViewController: UIViewController {
         
     }
     
+    // Uno cada boton de la vista con una funcion que se va a ejecutar cada vez que pulse el boton, los cuatros botones hacen lo mismo, lo unico en que se diferencian es a que boton le cambian el color, voy a explicar como funciona uno de los botones.
+    
+    // Cuando pulso el primer boton llamo a la funcion changeColor1
     @IBAction func changeColor1() {
         
-        if isPressedButton1 {
-            if isClockwise {
+        // Compruebo si debo cambiar los colores en sentido de las agujas del reloj
+        if isClockwise {
+            // En el caso de que tenga que cambiar los colores en sentido de las agujas del reloj, compruebo si el siguiente boton tiene el color que tenia originalmente o el nuevo color
+            if isOriginalColor2 {
+                // Si tiene el color original, se lo cambio al nuevo color que le quiera dar
                 button2.backgroundColor = .systemRed
             } else {
-                button4.backgroundColor = .systemYellow
+                // En el caso de que no tenga el color original, le cambio el color al original
+               button2.backgroundColor = .systemOrange
             }
-        } else {
-            if isClockwise {
-                button2.backgroundColor = .systemOrange
+            // Cambio el estado de la variable isOriginalColor para marcar si el boton tiene el color original o el nuevo color
+            isOriginalColor2.toggle()
+        } else  {
+            // En el caso de que tenga que cambiar los colores en sentido contrario al de las agujas del reloj, hago lo mismo, pero con el boton anterior
+            if isOriginalColor4 {
+                button4.backgroundColor = .systemYellow
             } else {
                 button4.backgroundColor = .systemPink
             }
             
+            isOriginalColor4.toggle()
         }
         
-        isPressedButton1.toggle()
     }
     
     @IBAction func changeColor2() {
         
-        if isPressedButton2 {
-            if isClockwise {
+        if isClockwise {
+            if isOriginalColor3 {
                 button3.backgroundColor = .systemTeal
             } else {
-                button1.backgroundColor = .systemGreen
-            }
-        } else {
-            if isClockwise {
                 button3.backgroundColor = .systemPurple
+            }
+            
+            isOriginalColor3.toggle()
+        } else {
+            if isOriginalColor1 {
+                button1.backgroundColor = .systemGreen
             } else {
                 button1.backgroundColor = .systemIndigo
             }
             
+            isOriginalColor1.toggle()
         }
-        
-        isPressedButton2.toggle()
+
     }
     
    
     @IBAction func changeColor3() {
         
-        if isPressedButton3 {
-            if isClockwise {
+        if isClockwise {
+            if isOriginalColor4 {
                 button4.backgroundColor = .systemYellow
             } else {
-                button2.backgroundColor = .systemRed
-            }
-               } else {
-            if isClockwise {
                 button4.backgroundColor = .systemPink
+            }
+            
+            isOriginalColor4.toggle()
+               } else {
+            if isOriginalColor2 {
+                button2.backgroundColor = .systemRed
             } else {
                 button2.backgroundColor = .systemOrange
             }
+            
+            isOriginalColor2.toggle()
                }
-               
-            isPressedButton3.toggle()
     }
     
    
     @IBAction func changeColor4() {
         
-        if isPressedButton4 {
-            if isClockwise {
+        if isClockwise {
+            if isOriginalColor1 {
                 button1.backgroundColor = .systemGreen
             } else {
-                button3.backgroundColor = .systemTeal
-            }
-               } else {
-            if isClockwise {
                 button1.backgroundColor = .systemIndigo
+            }
+            
+            isOriginalColor1.toggle()
+               } else {
+            if isOriginalColor3 {
+                button3.backgroundColor = .systemTeal
             } else {
                 button3.backgroundColor = .systemPurple
             }
+            
+            isOriginalColor3.toggle()
                }
-               
-            isPressedButton4.toggle()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Cuando carga la vista cambio el texto de la variable infoLabel segun la variable isClockwise
         if isClockwise {
             infoLabel.text = "Go Clockwise"
         } else {
